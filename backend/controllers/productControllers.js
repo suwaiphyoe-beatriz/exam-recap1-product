f6afd2cab8249ccf6d4e237483035a247ae029f1
Yicheng CHEN <yicheng4499@gmail.com>
2025-10-01 11:39:50 +0300

    Now we really finished Step 1: Adding and Fetching Products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,12 +3,24 @@ const mongoose = require("mongoose");
 
 //GET / products;
 const getAllProducts = async (req, res) => {
-  res.send("getAllProducts");
+  try {
+    const products = await Product.find({}).sort({ createdAt: -1 });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve products" });
+  }
 };
 
 // POST /products
 const createProduct = async (req, res) => {
-  res.send("createProduct");
+  try {
+    const newProduct = await Product.create({ ...req.body });
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Failed to create product", error: error.message });
+  }
 };
 
 // GET /products/:productId
