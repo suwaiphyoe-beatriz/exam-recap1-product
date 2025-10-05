610e0303fb3b3d9f7ef16d6655f0da56610989b6
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-10-05 03:59:06 +0300

    updated for deployment

--- backend/app.js
+++ backend/app.js
@@ -12,15 +12,18 @@ app.use(cors())
 app.use(express.json());
 
 connectDB();
-
+app.use(express.static('view'));
 // Use the productRouter for all "/products" routes
 app.use("/api/products", productRouter);
 // Use the userRouter for all "/users" routes
 app.use("/api/users", userRouter);
 
-app.use(unknownEndpoint);
+app.use('/api',unknownEndpoint);
 app.use(errorHandler);
 
+app.use((req, res) => {
+  res.sendFile(__dirname + '/view/index.html');
+});
 module.exports = app;
 
 // app.listen(process.env.PORT, () => {
